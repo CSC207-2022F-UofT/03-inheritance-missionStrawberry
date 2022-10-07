56eb7bb8d87804bf2cb4143957035cc2e1cc2bb5
missionStrawberry <annam7245@sd61learn.ca>
2022-10-07 15:11:51 -0400

    completed todo in HandBag class

--- src/main/java/HandBag.java
+++ src/main/java/HandBag.java
@@ -12,8 +12,8 @@ public class HandBag extends Bag{
      * Creates a new HandBag with the given color and
      * capacity.
      *
-     * @param color
-     * @param capacity
+     * @param color : color of the bag
+     * @param capacity : capacity of the bag
      */
     public HandBag(String color, int capacity) {
         /**
@@ -28,11 +28,12 @@ public class HandBag extends Bag{
      */
     @Override
     public void enhance() {
-        /* TODO: Implement this method.
+        /*  Implement this method.
          *       You may want to use the increaseCapacity() method that was
          *       implemented in Bag.
          *
          * To call a method defined in a parent, you use super.method_name(...)
          */
+        super.increaseCapacity(1);
     }
 }
